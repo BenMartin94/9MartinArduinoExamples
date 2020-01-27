7fbfcdf1b737f4cf5081247738f91bb3d47b3f7c
Ben <benjosephmartin@gmail.com>
2020-01-26 19:11:32 -0600

    Added template

---
+++ MotorAndHumiditySensorTemplate/MotorAndHumiditySensorTemplate.ino
@@ -0,0 +1,36 @@
+#include "Servo.h"
+
+Servo motor;
+
+int analogInputPin;; // this is the pin for analog input
+int voltageInputVal; 
+
+int motorPin;
+
+bool gateOpen;
+
+void setup() {
+  // put your setup code here, to run once:
+  analogInputPin = A2;//insert appropriate pin for HS
+  motorPin = 3;//insert appropriate value for motor control
+  motor.attach(motorPin);
+  gateOpen = false;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  voltageInputVal = analogRead(analogInputPin);
+
+  if(voltageInputVal<20/*insert minimum threshold here in place of 20*/&&!gateOpen){
+    //insert code open gate
+    
+    gateOpen = true;
+  }
+  if(voltageInputVal>1000/*insert minimum threshold here in place of 20*/&&gateOpen){
+    //inser code to close gate
+    
+    gateOpen = false;
+  }
+
+}
